5141cd7fdeed69ae84349fb3e6b9211e40600d82
Kristian Utsar <78056935+Utsar@users.noreply.github.com>
2021-07-22 11:04:34 +0100

    hello, itrs me

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 // main page is not useed right now;
-import Profile from "./components/Profile/Profile.jsx"
+import Profile from "./components/Profile/Profile.jsx";
 import ProfilePage from "./components/Profile/ProfilePage.jsx";
 import MyNav from "./components/MyNav/MyNav";
 import MyFooter from "./components/MyFooter/MyFooter";
 import LoginPage from "./components/LoginPage";
 import HomePage from "./components/HomePage/HomePage";
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import NetworkFeed from "./components/Network/NetworkFeed";
 import People from "./components/PeopleProfile/People";
-import {Component} from "react";
+import { Component } from "react";
+// Hello, it's me !
 
-const {REACT_APP_BACKEND_URL} = process.env;
+const { REACT_APP_BACKEND_URL } = process.env;
 class App extends Component {
-
   state = {
-    user: "", 
+    user: "",
     isLoading: "",
-    img:""
+    img: "",
   };
-  
+
   profile = (pic) => {
     this.setState({
       ...this.state,
-      img: pic
+      img: pic,
     });
-  };  
+  };
 
   componentDidMount = async () => {
-    this.setState({isLoading: true});
+    this.setState({ isLoading: true });
     try {
       const response = await fetch(
-        `${REACT_APP_BACKEND_URL}/profile/60f67bd86bce175ba8dec1d7`);
+        `${REACT_APP_BACKEND_URL}/profile/60f67bd86bce175ba8dec1d7`
+      );
       if (response.ok) {
         const data = await response.json();
         console.log("This is sidebar profile data", data);
-        this.setState({user: data, isLoading: false});
+        this.setState({ user: data, isLoading: false });
       }
     } catch (error) {
       console.log(error);
@@ -57,18 +58,18 @@ class App extends Component {
               <HomePage />
             </Route>
             <Route
-                exact
-                path='/profile/:id'
-                render={(routerProps) => (
-                  <Profile {...routerProps} /* profilePic={this.profile} */ />
-                )}
-              />
+              exact
+              path="/profile/:id"
+              render={(routerProps) => (
+                <Profile {...routerProps} /* profilePic={this.profile} */ />
+              )}
+            />
             {/* <Route exact path="/profile/:id" component={ProfilePage} /> */}
             <Route
-                exact
-                path='/profile/:username/:userId'
-                render={(routerProps) => <People {...routerProps} />}
-              />
+              exact
+              path="/profile/:username/:userId"
+              render={(routerProps) => <People {...routerProps} />}
+            />
           </Switch>
           <MyFooter />
         </Router>
